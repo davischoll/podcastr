7cc29894c005493b738c91d5a58297ca44170866
Davi Scholl <davis@bandavide.com.br>
2021-04-30 00:47:24 -0300

    Criando Api e formatando dados enviados para a Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -50,14 +50,14 @@
     ir até a API e buscar as informações.
 */
 
-export default function Home(props) { 
-  return (
-    <div>
-      <h1>Index</h1>
-      <p>{JSON.stringify(props.episodes)}</p> 
-    </div>
-  )
-}
+// export default function Home(props) { 
+//   return (
+//     <div>
+//       <h1>Index</h1>
+//       <p>{JSON.stringify(props.episodes)}</p> 
+//     </div>
+//   )
+// }
 
 // export async function getServerSideProps() {
   
@@ -92,14 +92,86 @@ export default function Home(props) {
     - rodar o projeto com $ yarn start
 */
 
-export async function getStaticProps() {
+// export default function Home(props) { 
+//   return (
+//     <div>
+//       <h1>Index</h1>
+//       <p>{JSON.stringify(props.episodes)}</p> 
+//     </div>
+//   )
+// }
+
+// export async function getStaticProps() {
   
-  const response = await fetch('http://localhost:3333/episodes')
-  const data = await response.json()
+//   const response = await fetch('http://localhost:3333/episodes')
+//   const data = await response.json()
+
+//   return {
+//     props: {
+//       episodes: data
+//     },
+//     revalidate: 60 * 60 * 8
+//   }
+// }
+
+
+import { GetStaticProps } from 'next'  // Tipagem da função, determina como é o formato da função, qual é o seu retorno etc...
+import { format, parseISO } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+import { api } from '../services/api'
+import { convertDurationToTimeString } from '../util/convertDurationToTimeString'
+
+type Episode = {
+  id: string
+  title: string
+  thumbnail: string
+  members: string
+  publishedAt: string
+  duration: number
+  durationAsString: string
+  description: string
+  url: string
+}
+
+type HomeProps = {
+  episodes: Array<Episode>   // Ou episodes: Episode[]
+}
+
+export default function Home(props: HomeProps) {
+  return (
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p> 
+    </div>
+  )
+}
+
+export const getStaticProps:GetStaticProps = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  })
+
+  const episodes = data.map(episode => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', { locale: ptBR }),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+      description: episode.description,
+      url: episode.file.url,
+    }
+  })
 
   return {
     props: {
-      episodes: data
+      episodes
     },
     revalidate: 60 * 60 * 8
   }
