916dd4aa91cf6a878a93bd135029c024a2cae3f3
Davi Scholl <davis@bandavide.com.br>
2021-04-29 22:57:32 -0300

    | SPA | SSR | SSG |

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,106 @@
-export default function Home() {
+// SPA => Single Page Application
+// SSR => Server Side Rendering
+// SSG => Static Site Generator
+
+/*
+  REQUISIÇÃO NO MODELO 'SPA':
+  Os dados que serão puxados da api, serão carregados somente no momento em que a pessoa acessa a tela da app
+  e o conteúdo da página será montado pelo javascrip do browser posteriormente.
+  Exemplo com uso de "React puro":
+
+   useEffect:
+   Uma função (hook) do React que dispara algo, sempre que algo mudar na aplicação.
+   É basicamente para isto: efeitos colaterais. Quando algo mudar na app, quero que alguma coisa aconteça
+    - 1o parâmetro: O QUE eu quero executar: () => {}
+    - 2o parâmetro: QUANDO: []  // um array q recebe qualquer variável
+      * Se uma variável é passada, pelo Home(variavel), para o array do useEffect, toda vez que essa variável mudar,
+        o código do primeiro parâmetro será executado novamente.
+      * No React, quando queremos que uma função execute uma única vez, assim que o componente for exibido em tela,
+        basta informar um array vazio [] no segundo parâmetro do useEffect.
+*/
+
+  // import { useEffect } from "react"
+
+  // export default function Home() {
+  //   useEffect(() => {
+  //     fetch('http://localhost:3333/episodes')
+  //       .then(response => response.json())
+  //       .then(data => console.log(data))
+  //   }, [])
+
+  //   return (
+  //     <h1>Index</h1>
+  //   )
+  // }
+
+/*
+  REQUISIÇÃO NO MODELO 'SSR':
+  Com o Next.js, basta exportar uma função chamada getServerSideProps()
+  Apenas exportando essa função, o Next já entende que ele precisa executá-la antes de exibir o conteúdo da
+  página Home() para o usuário final.
+    - Transformamos essa função em async e ela retorna um objeto, que contem o dado 'props', onde especificamos
+      quais dados queremos puxar.
+    - É preciso sempre retornar "props"!
+    - Tudo o que retornamos de "props", será repassado para o componente Home(props)
+  
+  OBS:
+  DIFERENÇA DO SSR P/ SSG:
+    getServerSideProps() será executado todas as vezes que alguém acessar a home da aplicação.
+    Mas se a home não sofre alterações constantes, a toda hora, não se faz necessário todas as vezes
+    ir até a API e buscar as informações.
+*/
+
+export default function Home(props) { 
   return (
-    <h1>Index</h1>
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p> 
+    </div>
   )
+}
+
+// export async function getServerSideProps() {
+  
+//   const response = await fetch('http://localhost:3333/episodes')
+//   const data = await response.json()
+
+//   return {
+//     props: {
+//       episodes: data
+//     }
+//   }
+// }
+
+
+/*
+  REQUISIÇÃO NO MODELO SSG:
+  Assim que uma pessoa acessa a página, uma versão estática dela é gerada, e essa página (que é um HTML puro),
+  será servida para todas as pessoas que acessarem aquela mesma página depois dessa primeira. Todas estarão
+  visualizando exatamente o mesmo conteúdo. Não é necessário fazer uma nova requisição para a Api.
+
+  - Para usar SSG, basta trocar o nome do método para getStaticProps(). Não é necessário fazer mais nada!
+
+  O que pode-se fazer é passar mais uma opção, abaixo de props, chamada 'revalidate':
+    - "revalidate" recebe um número, em segundos, que diz de quanto em quanto tempo queremos gerar uma nova
+      versão desta página. (Ex: 60s * 60 * 8) = 3x por dia.
+    Todas as pessoas que acessarem nesse intervalo de tempo, vão consumir o html estático dessa página.
+  
+  OBS:
+    Este recurso de SSG só funciona em produção.
+    Então é necessário gerar uma build do projeto, para simular que estamos com a app rodando em produção:
+    - executar $ yarn build
+    - rodar o projeto com $ yarn start
+*/
+
+export async function getStaticProps() {
+  
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data
+    },
+    revalidate: 60 * 60 * 8
+  }
 }
